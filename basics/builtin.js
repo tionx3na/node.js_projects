77c9a425dccb89e146d5f2f0d7314fb3f202e632
xena <1runx3na2@gmail.com>
2021-12-01 20:03:23 +0530

    Project1:task manager -- routes

---
+++ basics/builtin.js
@@ -0,0 +1,6 @@
+// Basic Built-in modules
+// os module
+import { userInfo } from 'os'
+
+const user = userInfo()
+console.log(user)
--- task_manager/app.js
+++ task_manager/app.js
@@ -1,1 +1,19 @@
-console.log('Task Manager App')
+import express from 'express';
+import router from './routes/tasks.js';
+
+const app = express()
+const port = 3000
+
+// middleware
+
+app.use(express.json())
+
+// server listen
+
+app.listen(port, () => {
+    console.log('lisening on port: '+port)
+})
+
+//routes 
+
+app.use('/api/v1/tasks', router)
